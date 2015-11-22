d9e41701adc6a003306d6879cee57ba98736659a
Diego Concha <dconcha@datto.com>
2015-11-22 13:45:11 -0500

    basic ng2 alpha 46 file structure based on angular docs

---
+++ src/app/app.ts
@@ -0,0 +1,9 @@
+import {bootstrap, Component} from 'angular2/angular2';
+
+@Component({
+    selector: 'tracker-app',
+    template: '<h1>Tracker App</h1>'
+})
+class AppComponent { }
+
+bootstrap(AppComponent);
